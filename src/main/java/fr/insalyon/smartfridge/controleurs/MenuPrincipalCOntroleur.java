d2c5d284b7311b1ff4d207be6dcfc3b1910b6548
Elsa Berthaut <elsa.berthaut@gmail.com>
2014-04-13 21:02:43 +0200

    Rename

--- src/main/java/fr/insalyon/smartfridge/controleurs/MenuPrincipalCOntroleur.java
+++ src/main/java/fr/insalyon/smartfridge/controleurs/MenuPrincipalCOntroleur.java
@@ -1,10 +1,13 @@
 package fr.insalyon.smartfridge.controleurs;
 
+import fr.insalyon.smartfridge.services.ServiceAlerte;
+import fr.insalyon.smartfridge.services.ServiceThermodynamique;
 import fr.insalyon.smartfridge.vues.EntreeCategories;
 import fr.insalyon.smartfridge.vues.Fenetre;
 import fr.insalyon.smartfridge.vues.MenuPrincipal;
 import fr.insalyon.smartfridge.vues.SortieAliment;
 
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -25,5 +28,25 @@ public class MenuPrincipalControleur implements ActionListener {
         } if(e.getSource().equals(menu.getRetirerButton())) {
             fenetre.allerA(new SortieAliment(fenetre));
         }
+    }
+
+    public void gererAlerte() {
+        int status = ServiceAlerte.statusAlerte(2);
+        switch(status) {
+            case 1:
+                menu.getAlerteButton().setEnabled(true);
+                menu.getAlerteButton().setForeground(new Color(200, 150, 0));
+                menu.getAlerteButton().setText("Des aliments sont proches de la peremption");
+                break;
+            case 2:
+                menu.getAlerteButton().setEnabled(true);
+                menu.getAlerteButton().setForeground(new Color(200, 0, 0));
+                menu.getAlerteButton().setText("Des aliments sont perimes");
+                break;
+        }
+    }
+
+    public void gererTemperature() {
+        menu.getTemperatureLabel().setText(ServiceThermodynamique.mettreAJourTemperature() + " degres Celcius");
     }
 }
