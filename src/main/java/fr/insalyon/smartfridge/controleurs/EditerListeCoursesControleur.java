2bc133b3db7542e8aceaa995e464d8c02d23909e
Elsa Berthaut <elsa.berthaut@gmail.com>
2014-05-30 19:49:33 +0200

    Imprime 1 page !

--- src/main/java/fr/insalyon/smartfridge/controleurs/EditerListeCoursesControleur.java
+++ src/main/java/fr/insalyon/smartfridge/controleurs/EditerListeCoursesControleur.java
@@ -2,12 +2,14 @@ package fr.insalyon.smartfridge.controleurs;
 
 import fr.insalyon.smartfridge.modeles.Aliment;
 import fr.insalyon.smartfridge.services.ServiceCourses;
+import fr.insalyon.smartfridge.services.ServiceImpression;
 import fr.insalyon.smartfridge.vues.ChangerHabitude;
 import fr.insalyon.smartfridge.vues.EditerListeCourses;
 import fr.insalyon.smartfridge.vues.Fenetre;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.List;
 
 /**
  * Created by fannygallais on 27/04/2014.
@@ -16,6 +18,7 @@ public class EditerListeCoursesControleur implements ActionListener{
 
     private Fenetre fenetre;
     private EditerListeCourses editerListeCourses;
+    private List<Aliment> aliments;
 
     public EditerListeCoursesControleur(Fenetre fenetre, EditerListeCourses editerListeCourses){
         this.fenetre = fenetre;
@@ -28,14 +31,14 @@ public class EditerListeCoursesControleur implements ActionListener{
     public void actionPerformed(ActionEvent e) {
         if (e.getSource().equals(editerListeCourses.getChangerHabitudesButton())) {
             fenetre.allerA(new ChangerHabitude(fenetre));
-        } else if (e.getSource().equals(editerListeCourses.getReinitialiserButton())) {
-            fenetre.allerA(new EditerListeCourses(fenetre));
+        } else if (e.getSource().equals(editerListeCourses.getImpressionButton())) {
+            ServiceImpression.imprimer(aliments);
         }
     }
 
     public void creerListe() {
-        editerListeCourses.getListeDeCourses().setModel(new ListModel<Aliment>(ServiceCourses.genererListeCourses()));
-
+        aliments  =ServiceCourses.genererListeCourses();
+        editerListeCourses.getListeDeCourses().setModel(new ListModel<Aliment>(aliments));
     }
 
     public void rafraichirListe() {
